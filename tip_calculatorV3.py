cdd02c48e93f53fe7dde26111f2c66e39b8dbded
Shaun <shaunktw@gmail.com>
2014-01-12 14:43:40 -0500

    Updated tip calculator to have user supplied variables using sysarvg

---
+++ tip_calculatorV3.py
@@ -0,0 +1,22 @@
+import sys
+
+#Assigning variables
+
+meal_input = sys.argv[1]
+tax_input  = sys.argv[2]
+tip_input  = sys.argv[3]
+
+meal = float(meal_input)
+tax  = float(tax_input)/100
+tip  = float(tip_input)/100
+
+tax_value = tax * meal
+meal_with_tax = meal + tax_value
+tip_value = meal_with_tax * tip
+
+total = meal_with_tax + tip_value
+
+print "The base cost of your meal was $%.2f" % (meal)
+print "You need to pay $%.2f" % (tax_value)
+print "Tipping at a rate of %d%%, you should leave $%.2f for a tip." % (tip * 100, tip_value)
+print "The grand total of your meal is $%.2f" % (total)
