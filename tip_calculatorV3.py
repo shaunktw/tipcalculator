ede12f6d68727db1b089aa61af5f690a6900dcbb
Shaun <shaunktw@gmail.com>
2014-01-12 20:24:08 -0500

    Modified tip calculator to accept input using optparser

--- tip_calculatorV3.py
+++ tip_calculatorV3.py
@@ -1,22 +1,24 @@
 import sys
+from optparse import OptionParser
 
 #Assigning variables
+parser = OptionParser()
+parser.add_option("-m", "--meal", dest = "meal", type = "float", help = "the base cost of the meal")
+parser.add_option("-x", "--tax", dest = "tax", type= "float",  help = "the raw tax value")
+parser.add_option("-t", "--tip", dest = "tip", type = "float", help = "the tip that is required", default = 0.20)
 
-meal_input = sys.argv[1]
-tax_input  = sys.argv[2]
-tip_input  = sys.argv[3]
+(options, args) = parser.parse_args()
+if not (options.meal and options.tax):
+	parser.error("You need to supply the meal price and the tax")
 
-meal = float(meal_input)
-tax  = float(tax_input)/100
-tip  = float(tip_input)/100
-
-tax_value = tax * meal
-meal_with_tax = meal + tax_value
-tip_value = meal_with_tax * tip
+tax_value = (options.tax/100) * options.meal
+meal_with_tax = options.meal + tax_value
+tip_value = meal_with_tax * (options.tip/100)
 
 total = meal_with_tax + tip_value
 
-print "The base cost of your meal was $%.2f" % (meal)
-print "You need to pay $%.2f" % (tax_value)
-print "Tipping at a rate of %d%%, you should leave $%.2f for a tip." % (tip * 100, tip_value)
-print "The grand total of your meal is $%.2f" % (total)
+
+print "The base cost of your meal was ${0:.2f}".format(options.meal)
+print "You need to pay ${0:.2f} for tax".format(tax_value)
+print "Tipping at a rate of {0:.2f}%, you should leave ${1:.2f} for a tip.".format(options.tip, tip_value)
+print "The grand total of your meal is ${0:.2f}".format(total)
